2a2db5e836ae6cce55d80c4ba595142209855aa3
natrux <tmpdf1f71a7@tefrod.de>
2024-11-02 10:02:08 +0100

    resolve file name conflict

--- include/asgard/core/exception.h
+++ include/asgard/core/exception.h
@@ -9,12 +9,26 @@ namespace asgard{
 namespace core{
 
 
+// Implementation in the header just this once so that we don't need exception.cpp
+// which would collide with Exception.cpp on some operating systems.
 class exception : public std::exception{
 public:
-	exception(std::shared_ptr<const data::Exception> error);
+	exception(std::shared_ptr<const data::Exception> error):
+		exception_object(error)
+	{
+		if(!exception_object){
+			throw std::logic_error("Invalid exception object");
+		}
+		error_string = exception_object->what();
+	}
 
-	const char *what() const noexcept override;
-	std::shared_ptr<const data::Exception> get_exception() const;
+	const char *what() const noexcept override{
+		return error_string.c_str();
+	}
+
+	std::shared_ptr<const data::Exception> get_exception() const{
+		return exception_object;
+	}
 
 private:
 	std::string error_string;
