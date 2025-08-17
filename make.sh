ace25aa69fdb14cf9875af961e70ddecfab5703f
natrux <tmpdf1f71a7@tefrod.de>
2025-08-17 11:16:46 +0200

    using nproc to compile in parallel

--- make.sh
+++ make.sh
@@ -11,5 +11,11 @@ while [ -n "$1" ]; do
 done
 
 
+parallel=1
+if command -v nproc >/dev/null 2>&1; then
+	parallel=$(nproc)
+fi
+
 cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -B build/ .
-cmake --build build/ --parallel 2
+cmake --build build/ --parallel $parallel
+
