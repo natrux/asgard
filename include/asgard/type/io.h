e08dece1ca3a0f8e0b5c034f43c40c1d50c92b18
natrux <tmpdf1f71a7@tefrod.de>
2026-02-12 22:22:06 +0100

    type file writing

---
+++ include/asgard/type/io.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <asgard/fs/File.h>
+
+
+namespace asgard{
+namespace type{
+
+
+template<class T>
+void write_to_file(const T &value, const std::string &path){
+	const fs::File file(path);
+	auto source = file.output_source();
+	io::TypeWriter writer(source);
+	writer.write_type(value);
+}
+
+
+template<class T>
+void read_from_file(T &value, const std::string &path){
+	const fs::File file(path);
+	auto source = file.input_source();
+	io::TypeReader reader(source);
+	reader.read_type(value);
+}
+
+
+template<class T>
+T read_from_file(const std::string &path){
+	T value;
+	read_from_file(value, path);
+	return value;
+}
+
+
+template<class T>
+std::vector<uint8_t> write_to_vector(const T &value){
+	auto source = std::make_shared<io::VectorOutputSource>();
+	{
+		io::TypeWriter writer(source);
+		writer.write_type(value);
+	}
+	return source->get();
+}
+
+
+template<class T>
+void read_from_vector(T &value, const std::vector<uint8_t> &vector){
+	auto source = std::make_shared<asgard::io::VectorInputSource>(vector);
+	asgard::io::TypeReader reader(source);
+	reader.read_type(value);
+}
+
+
+template<class T>
+T read_from_vector(const std::vector<uint8_t> &vector){
+	T value;
+	read_from_vector(value, vector);
+	return value;
+}
+
+
+}
+}
--- test/type-io/src/main.cpp
+++ test/type-io/src/main.cpp
@@ -9,21 +9,18 @@
 #include <asgard/data/DataPacket.hxx>
 #include <asgard/data/log_level_e.hxx>
 
+#include <asgard/type/io.h>
+
 #include <iostream>
 #include <iomanip>
 
 
 template<class T>
 std::vector<uint8_t> write_to_vector(const T &data_out){
-	auto out = std::make_shared<asgard::io::VectorOutputSource>();
 	const auto t_w = asgard::time::now();
-	{
-		asgard::io::TypeWriter writer(out);
-		writer.write_type(data_out);
-	}
+	const auto vector = asgard::type::write_to_vector(data_out);
 	const auto d_w = asgard::time::since(t_w);
 
-	const auto vector = out->get();
 	const size_t max_output = 128;
 	for(size_t i=0; i<std::min<size_t>(vector.size(), max_output); i++){
 		const auto &byte = vector[i];
@@ -41,13 +38,8 @@ std::vector<uint8_t> write_to_vector(const T &data_out){
 
 template<class T>
 T read_from_vector(const std::vector<uint8_t> &vector){
-	typename std::remove_cv<typename std::remove_reference<T>::type>::type data_in;
-	auto in = std::make_shared<asgard::io::VectorInputSource>(vector);
 	const auto t_r = asgard::time::now();
-	{
-		asgard::io::TypeReader reader(in);
-		reader.read_type(data_in);
-	}
+	const auto data_in = asgard::type::read_from_vector<typename std::remove_cv<typename std::remove_reference<T>::type>::type>(vector);
 	const auto d_r = asgard::time::since(t_r);
 	std::cout << vector.size() << " bytes read in " << asgard::time::strtime(d_r) << std::endl;
 
@@ -56,9 +48,22 @@ T read_from_vector(const std::vector<uint8_t> &vector){
 
 
 template<class T>
-void test_by_value(const T &data_out){
+void test_by_vector(const T &data_out){
 	const auto vector = write_to_vector(data_out);
 	const auto data_in = read_from_vector<T>(vector);
+
+	const bool equal = (data_in == data_out);
+	if(!equal){
+		throw std::runtime_error("Not equal");
+	}
+}
+
+
+template<class T>
+void test_by_file(const T &data_out){
+	const std::string path = "tmp.dat";
+	asgard::type::write_to_file(data_out, path);
+	const auto data_in = asgard::type::read_from_file<T>(path);
 
 	const bool equal = (data_in == data_out);
 	if(!equal){
@@ -121,12 +126,18 @@ void test_by_bin(const T &data_out){
 }
 
 
+template<class T>
+void test_by(const T &data_out){
+	test_by_vector(data_out);
+	test_by_file(data_out);
+	test_by_bin(data_out);
+}
+
 
 void test(){
 	{
 		const float number = 13.37;
-		test_by_value(number);
-		test_by_bin(number);
+		test_by(number);
 	}
 
 	{
@@ -135,16 +146,14 @@ void test(){
 			{{"World", 23}, {-3.14, -2.71828}},
 			{{"Foobar", -1337}, {-4.5, -1.9, 0, 1.9, 4.5}},
 		};
-		test_by_value(data);
-		test_by_bin(data);
+		test_by(data);
 	}
 
 	{
 		const std::vector<std::optional<int32_t>> data = {
 			-42, {}, -23,
 		};
-		test_by_value(data);
-		test_by_bin(data);
+		test_by(data);
 	}
 
 	{
@@ -154,8 +163,7 @@ void test(){
 			asgard::data::log_level_e::WARN,
 			asgard::data::log_level_e::ERROR,
 		};
-		test_by_value(data);
-		test_by_bin(data);
+		test_by(data);
 	}
 
 	{
@@ -165,8 +173,7 @@ void test(){
 			{asgard::data::log_level_e::WARN, "warning log message"},
 			{asgard::data::log_level_e::ERROR, "error log message"},
 		};
-		test_by_value(data);
-		test_by_bin(data);
+		test_by(data);
 	}
 
 	{
@@ -179,14 +186,12 @@ void test(){
 			}
 			data[i] = packet;
 		}
-		test_by_value(data);
-		test_by_bin(data);
+		test_by(data);
 	}
 
 	{
 		const std::vector<std::string> letters = {"Alpha", "Beta", "Gamma", "Delta"};
-		test_by_value(letters);
-		test_by_bin(letters);
+		test_by(letters);
 	}
 
 	{
@@ -203,8 +208,9 @@ void test(){
 			}
 		}
 
-		const auto vector = write_to_vector(data);
-		const auto result = read_from_vector<std::vector<std::shared_ptr<asgard::data::Value>>>(vector);
+		asgard::type::write_to_file(data, "tmp.dat");
+		const auto result = asgard::type::read_from_file<std::vector<std::shared_ptr<asgard::data::Value>>>("tmp.dat");
+
 		const size_t size = data.size();
 		if(result.size() != size){
 			throw std::runtime_error("Not equal");
